642421f816fd700b37501c7e992215baae8b18df
kjh2159 <kjh2159@dgist.ac.kr>
2025-09-16 10:14:26 +0900

    fix: script for jolt.sh

--- scripts/jolt.sh
+++ scripts/jolt.sh
@@ -1,3 +1,6 @@
+# screen off
+su -c "echo 0 > /sys/class/backlight/panel0-backlight/brightness"
+
 # silver cores off
 su -c "echo 0 > /sys/devices/system/cpu/cpu1/online"
 su -c "echo 0 > /sys/devices/system/cpu/cpu2/online"
@@ -10,5 +13,8 @@ su -c "echo 0 > /sys/devices/system/cpu/cpu3/online"
     -o output \
     --cpu-clock $1 \
     --ram-clock $2 \
-    --pulse-cpu-clock 0 \
-    --pulse-ram-clock 0
+    --pulse-cpu-clock $3 \
+    --pulse-ram-clock $4
+
+# screen on
+su -c "echo 1023 > /sys/class/backlight/panel0-backlight/brightness"
